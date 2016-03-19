7436a5ac4e98b75bf4500c7de4134bbc5e10dcac
Ben Mordue <bmordue@adobe.com>
2016-03-19 12:06:45 +0000

    Simple API

---
+++ fetch.js
@@ -0,0 +1,51 @@
+var needle = require('needle');
+var htmlparser = require('htmlparser');
+
+function constructUrl(asin) {
+	return "http://www.amazon.co.uk/gp/aw/s/ref=is_box_?k=" + asin; // mobile site
+}
+
+function parsePage(html, callback) {
+	var handler = new htmlparser.DefaultHandler(function (error, dom) {
+		if (error) {
+			return callback(error);
+		}
+		if (!dom) {
+			return callback(new Error("No DOM object!"));
+		}
+		var price;
+		try {
+			var bolds = htmlparser.DomUtils.getElementsByTagName("b", dom);
+			price = bolds[2].children[0].data;
+		} catch (e) {
+			return callback(e);
+		}
+		if (price[0] != '£') {
+			return callback(new Error("Expected currency symbol to be £; got " + price[0]));
+		}
+		price = price.slice(1);
+		var result = {
+			price: price,
+			currency: "GBP"
+		};
+		callback(null, result);
+	});
+	var parser = new htmlparser.Parser(handler);
+	parser.parseComplete(html);
+}
+
+function fetchPriceForAsin(asin, callback){
+	needle.get(constructUrl(asin), function(err, response) {
+		if (err) {
+			return callback(err);
+		}
+		if (response.statusCode != 200) {
+			return callback(new Error("Expected status code 200; got " + response.statusCode));
+		}
+		parsePage(response.body, callback);		
+	});
+}
+
+module.exports = {
+	fetchPriceForAsin: fetchPriceForAsin
+};
---
+++ index.js
@@ -0,0 +1,64 @@
+var fetch        = require('./fetch');
+var finalhandler = require('finalhandler');
+var http         = require('http');
+var Router       = require('router');
+var url          = require('url');
+var util         = require('util');
+
+const PORT = 3000;
+
+function handleClientError(code, msg, req, res) {
+	console.log(util.format("%s %s %s - BAD REQUEST: %s", code, req.method, req.url, msg));
+	res.statusCode = code;
+	res.setHeader('Content-Type', 'application/json; charset=utf-8');
+	res.end(JSON.stringify({error: msg}));
+}
+
+function handleBadRequest(errorMsg, req, res) {
+	handleClientError(400, errorMsg, req, res);
+}
+
+function handleNotFound(req, res) {
+	handleClientError(404, "Resource not found", req, res);
+}
+
+var router = Router();
+router.get('/price', function (req, res) {
+
+	if (req.method != "GET") {
+		var errorMsg = "Method not accepted: " + req.method;
+		return handleBadRequest(errorMsg, req, res);
+	}
+	var asin;
+	try {
+		asin = url.parse(req.url, true).query.asin;
+	} catch (e) {
+		return handleBadRequest("Bad query string", req, res);
+	}
+	if (!asin) {
+		return handleBadRequest("No ASIN in query string", req, res);
+	}
+	fetch.fetchPriceForAsin(asin, function(err, result) {
+		if (err) {
+			console.log(err);
+			return handleNotFound(req, res);
+		}
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		var responseJson = {
+			price: result.price,
+			currency: result.currency
+		};
+		var responseBody = JSON.stringify(responseJson, null, 4) + '\n';
+		res.end(responseBody);
+		console.log(util.format("%s %s %s", res.statusCode, req.method, req.url));
+	});
+
+});
+ 
+var server = http.createServer(function(req, res) {
+	router(req, res, finalhandler(req, res));
+});
+ 
+server.listen(PORT);
+console.log("Server price-to-asin listening on port " + PORT);
