4573906bcab70765213e004df338f3fb9c5b80f1
Ben Mordue <bmordue@adobe.com>
2016-06-02 16:25:01 +0100

    Dump HTML to file on parse error

--- fetch.js
+++ fetch.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var needle = require('needle');
 var htmlparser = require('htmlparser');
 var util = require('util');
@@ -6,23 +7,33 @@ function constructUrl(asin) {
 	return "http://www.amazon.co.uk/gp/aw/s/ref=is_box_?k=" + asin; // mobile site
 }
 
+function dumpHtml(html, error, callback) {
+	var filename = (new Date()).toISOString() + '_dump.html';
+	fs.writeFile(filename, html, function(err) {
+		if (err) {
+			console.log('Error writing file ' + filename + '\n' + util.inspect(err));
+		}
+		callback(error);
+	});
+}
+
 function parsePage(html, callback) {
 	var handler = new htmlparser.DefaultHandler(function (error, dom) {
 		if (error) {
-			return callback(error);
+			return dumpHtml(html, error, callback);
 		}
 		if (!dom) {
-			return callback(new Error("No DOM object!"));
+			return dumpHtml(html, new Error("No DOM object!"), callback);
 		}
 		var price;
 		try {
 			var bolds = htmlparser.DomUtils.getElementsByTagName("b", dom);
 			price = bolds[2].children[0].data;
 		} catch (e) {
-			return callback(e);
+			return dumpHtml(html, e, callback);
 		}
 		if (price[0] != '£') {
-			return callback(new Error("Expected currency symbol to be £; got " + price[0]));
+			return dumpHtml(html, new Error("Expected currency symbol to be £; got " + price[0]), callback);
 		}
 		price = price.slice(1);
 		var result = {
