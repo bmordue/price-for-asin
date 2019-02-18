11797a4f976f40340af4edbfe849ed6a80a4c662
ben <benmordue@gmail.com>
2019-02-18 21:52:59 +0000

    Add a test

--- test/fetch.test.js
+++ test/fetch.test.js
@@ -1,9 +1,21 @@
-var fetch = require('../fetch');
+const fetch = require('../fetch');
+const assert = require('assert');
+const util = require('util');
 
 describe('fetch', function() {
     describe('#fetchPriceForAsin', function() {
         it('should call back without error for valid ASIN', function(done) {
             fetch.fetchPriceForAsin('B014V4DXMW', done);
         });
+
+        it('should call back with a price and currency for valid ASIN', function(done) {
+            fetch.fetchPriceForAsin('B014V4DXMW', function(err, result) {
+		if (err) { return done(err); }
+		assert.strictEqual(typeof result.price, 'number');
+		assert.strictEqual(result.currency, 'GBP');
+                done();
+	    });
+        });
     });
+
 });
